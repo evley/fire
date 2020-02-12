cf4b03377f246f9ba54c5abc3970bebd8bc18a29
tinybigideas <craig@tinybigideas.com>
2020-02-12 20:09:55 +0000

    Add linting and production build command

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "none",
+  bracketSpacing: true,
+  arrowParens: "always",
+  parser: "typescript"
+};
