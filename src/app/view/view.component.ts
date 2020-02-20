5ef6763d26798f9b05e9bd8964bf49db42dc9d81
tinybigideas <craig@tinybigideas.com>
2020-02-20 22:01:00 +0000

    Add basic d3 [WIP]

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,8 +1,8 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-// TODO:
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 
 import { CONSTANTS } from '../app.constant';
 import { DataItem } from './+data-item/data-item.interface';
@@ -15,6 +15,7 @@ const numberList = [
   CONSTANTS.financial.liability,
   CONSTANTS.financial.assets
 ];
+const displayAsCurrency = (num: number) => new CurrencyPipe('en-US').transform(num, ' ');
 
 @Component({
   templateUrl: './view.component.html',
@@ -58,6 +59,7 @@ export class ViewComponent implements OnInit {
   public ngOnInit(): void {
     if (this._hasData()) {
       this._setData();
+      this._renderBubbleChart();
     } else {
       this._goToDefault();
     }
@@ -155,5 +157,133 @@ export class ViewComponent implements OnInit {
 
   private _goToDefault(): void {
     this._router.navigate(['/']);
+  }
+
+  private _renderBubbleChart(): void {
+    const data = {
+      children: this.items
+    };
+
+    const width = document.body.clientWidth;
+    const height = document.body.clientHeight;
+
+    const bubble = d3
+      .pack()
+      .size([width, height])
+      .padding(8);
+
+    // TODO: Add resize watch to resize should user shrink screen
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    // TODO: Impact items which are negative are ignored, decide how to action
+    const root = d3
+      .hierarchy(data)
+      .sum((d: any) => d.impact)
+      .sort((a: any, b: any) => b.impact - a.impact);
+
+    bubble(root);
+
+    const node = svg
+      .selectAll('.data-item')
+      .data(root.children)
+      .enter()
+      .append('g')
+      .attr('class', (d: any) => `data-item ${this._dataItemClass(d.data)}`)
+      .attr('transform', (d: any) => 'translate(' + d.x + ' ' + d.y + ')');
+
+    // Add circle
+    node
+      .append('circle')
+      .attr('r', (d: any) => d.r)
+      .attr('class', 'data-item__circle');
+
+    // Add text
+    node
+      .filter((d: any) => !this._hasImage(d.data) && !this._hasIcon(d.data))
+      .append('text')
+      .text((d: any) => d.data.name.charAt(0))
+      .attr('class', 'data-item__circle__text')
+      .attr('dy', '.3em')
+      .style('text-anchor', 'middle')
+      .style('font-size', (d: any) => `${d.r}px`);
+
+    // Add image instead of text
+    node
+      .filter((d: any) => this._hasImage(d.data))
+      .append('image')
+      .attr('class', 'data-item__circle__image')
+      .attr('clip-path', (d: any) => `url(${d.data.image})`)
+      .attr('xlink:href', (d: any) => d.data.image)
+      .attr('x', (d: any) => -d.r * 0.7)
+      .attr('y', (d: any) => -d.r * 0.7)
+      .attr('height', (d: any) => d.r * 2 * 0.7)
+      .attr('width', (d: any) => d.r * 2 * 0.7);
+
+    // Add icon instead of text or image
+    node
+      .filter((d: any) => this._hasIcon(d.data))
+      .append('text')
+      .text((d: any) => d.data.icon)
+      .attr('class', 'material-icons data-item__circle__icon')
+      .attr('dy', '.3em')
+      .style('text-anchor', 'middle')
+      .style('font-size', (d: any) => `${d.r}px`)
+      .attr('y', (d: any) => d.r / 5);
+
+    // Add title
+    // TODO: Style title to pretty it up or use d3 tooltip plugin
+    node
+      .append('title')
+      .text(
+        (d: any) =>
+          `${d.data.name}\n-----------\n+ ${displayAsCurrency(
+            d.data.essential ? d.data.expenditure * CONSTANTS.fireMultiplier : 0
+          )} FIRE amount\n+ ${displayAsCurrency(d.data.income)} income\n+ ${displayAsCurrency(
+            d.data.expenditure
+          )} expenditure\n+ ${displayAsCurrency(d.data.liability)} liability\n+ ${displayAsCurrency(
+            d.data.assets
+          )} assets\n-----------\n${
+            this._hasNegativeImpact(d.data.impact) ? '+' : '-'
+          } ${this._yearsMonthsDays(d.data.impact)} ${
+            this._hasNegativeImpact(d.data.impact) ? 'to' : 'from'
+          } your FIRE age`
+      );
+  }
+
+  private _dataItemClass(item: DataItem): string {
+    const classStart = 'data-item--';
+    return `${classStart}${
+      this._hasNegativeImpact(item.impact) ? 'negative' : 'positive'
+    } ${classStart}${this._hasImage(item) ? 'has-image' : ''}`;
+  }
+
+  private _hasPositiveImpact(impact: number): boolean {
+    return impact < 0;
+  }
+
+  private _hasNegativeImpact(impact: number): boolean {
+    return impact > 0;
+  }
+
+  private _yearsMonthsDays(num: number): string {
+    const totalDays = Math.abs(num) * 365;
+    const years = Math.floor(totalDays / 365);
+    const months = Math.floor((totalDays - years * 365) / 30);
+    const days = Math.floor(totalDays - years * 365 - months * 30);
+    return `${years} year(s)${months > 0 ? ' ' + months + ' month(s)' : ''}${
+      days > 0 ? ' ' + days + ' day(s)' : ''
+    }`;
+  }
+
+  private _hasImage(item: DataItem): boolean {
+    return item.image && item.image.length > 1;
+  }
+
+  private _hasIcon(item: DataItem): boolean {
+    return item.icon && item.icon.length > 1 && !this._hasImage(item);
   }
 }
