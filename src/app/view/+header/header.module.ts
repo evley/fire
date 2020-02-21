c7a3fd8bf51488f246609a656cff9061f404e225
tinybigideas <craig@tinybigideas.com>
2020-02-21 21:37:34 +0000

    Refactor header to display appId and import only

--- src/app/view/+header/header.module.ts
+++ src/app/view/+header/header.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { ImporterModule } from '@evley/importer';
 
 import { HeaderComponent } from './header.component';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, ImporterModule],
+  imports: [CommonModule, ImporterModule],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
 })
